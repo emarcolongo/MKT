cfcad96f4b400276a2ab8444c29124a3e82399e3
emarcolongo <33271182+emarcolongo@users.noreply.github.com>
2020-05-29 12:40:49 -0300

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function($){
    //window.location.href = 'www.google.com';
-   alert('Abrir Pagina');
+   //alert('Abrir Pagina');
    window.location.href = 'https://www.hyson.info/meplaA';
 });
